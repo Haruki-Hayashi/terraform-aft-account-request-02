a1a25eef1429526e6da274480df1dd82f8e67393
HeXiaolu0601 <he.xiaolu@jp.panasonic.com>
2025-10-21 11:10:59 +0900

    add aws-students-2025-002

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,3 +109,31 @@ module "awsAccount2025-001" {
 
   account_customizations_name = "sandbox"
 }
+
+module "awsAccount2025-002" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-students-2025-002@ml.jp.panasonic.com"
+    AccountName               = "aws-students-2025-002"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws-selflearning-env@ml.jp.panasonic.com"
+    SSOUserFirstName          = "AWS Control Tower"
+    SSOUserLastName           = "Admin"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
